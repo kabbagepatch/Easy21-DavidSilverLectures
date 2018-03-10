d8668922e5afa9d8160b087ff12a02f36cc19405
Munjal <Kavish.Munjal@cerner.com>
2018-03-10 13:00:09 -0600

    Implemented Sarsa lambda

--- TDLambda.py
+++ TDLambda.py
@@ -7,11 +7,13 @@ HIT = 0
 STICK = 1
 
 
-class MonteCarlo:
-    def __init__(self):
+class TDLambda:
+    def __init__(self, lmbda=0.0):
         self.Q = np.random.randn(10, 22, 2)
         self.N = np.zeros([10, 22, 2])
         self.epsilon = 1.
+        self.lmbda = lmbda
+        self.gamma = 0.7
 
     def choose_action(self, state):
         random_action = random.randint(0, 1)
@@ -37,31 +39,40 @@ class MonteCarlo:
 
     def run_episode(self):
         game = Easy21()
+        E = np.zeros([10, 22, 2])
         action = self.choose_action(game.state)
         old_state = deepcopy(game.state)
         reward = 0
-        newQ = deepcopy(self.Q)
+
         while not game.state.game_over:
             reward = game.step(action)
+            new_action = self.choose_action(old_state)
 
             actual_total = old_state.player_total - 1
             if old_state.player_total < 1 or old_state.player_total > 21:
                 actual_total = 21
+            new_total = game.state.player_total - 1
+            if game.state.player_total < 1 or game.state.player_total > 21:
+                new_total = 21
 
+            delta = reward + \
+                self.gamma * self.Q[game.state.dealers_first_card - 1][new_total][new_action] - \
+                self.Q[old_state.dealers_first_card - 1][actual_total][action]
+
+            E[old_state.dealers_first_card - 1][actual_total][action] += 1
             self.N[old_state.dealers_first_card - 1][actual_total][action] += 1
             alpha = 1. / self.N[old_state.dealers_first_card - 1][actual_total][action]
-            newQ[old_state.dealers_first_card - 1][actual_total][action] += \
-                alpha * (reward - self.Q[old_state.dealers_first_card - 1][actual_total][action])
+
+            self.Q = self.Q + alpha * delta * E
+            E = self.gamma * self.lmbda * E
 
             self.epsilon = 100. / (100 + self.N[old_state.dealers_first_card - 1][actual_total][action])
-            action = self.choose_action(old_state)
             old_state = deepcopy(game.state)
-
-        self.Q = deepcopy(newQ)
+            action = new_action
         return reward
 
     def run_episodes(self, n):
-        for k in range(1,n+1):
+        for k in range(1, n+1):
             self.run_episode()
 
         return self.Q, self.N
--- main.py
+++ main.py
@@ -1,27 +1,24 @@
 from MonteCarlo import MonteCarlo
+from TDLambda import TDLambda
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cm
-from matplotlib.ticker import LinearLocator, FormatStrFormatter
+from plotQValues import plotQValues
 
-a = MonteCarlo()
-q, n = a.run_episodes(10000)
-Z = q[:, :, 0]
+mc = MonteCarlo()
+q_mc, n_mc = mc.run_episodes(100000)
 
-X = np.arange(22)
-Y = np.arange(10)
-X, Y = np.meshgrid(X, Y)
+mse = []
+lmbda = []
+for i in range(0, 10):
+    lmbda.append(i / 10.)
+    td = TDLambda(i / 10.)
+    q_td, n_td = td.run_episodes(10000)
 
-fig = plt.figure(figsize=(15, 10))
-ax = fig.gca(projection='3d')
+    error = (q_td - q_mc) ** 2
+    mse.append(sum(sum(sum(error))))
 
-surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False)
-
-ax.zaxis.set_major_locator(LinearLocator(10))
-ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-
-fig.colorbar(surf, shrink=0.5, aspect=5)
+plt.plot(lmbda, mse)
 
 plt.show()
+
+plotQValues(q_mc)
